b59b733ad318ac6be7964ad78244d2c611b106a2
vbreddyobulareddy <vbreddy.obulareddy@gmail.com>
2025-08-31 00:59:45 -0400

    Setting up React.JS Veerly-RealQ Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center">
+        <div className="flex items-center">
+          <h2>Veerly Welcome</h2>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
